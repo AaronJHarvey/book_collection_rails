fff42fc43f8de3e819dde586eb7d1bf286faaa69
AaronJHarvey <61170939+AaronJHarvey@users.noreply.github.com>
2020-09-08 19:46:36 -0400

    creates user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    if (user = User.create(user_params))#if the user_params entered are valid set user as the user being created
+      #make make the session[:id] the user.id
+      #redirect to the user (user_path(user))
+    else
+      #render :new
+    end
+
+  end
+
   def show
     @User = User.find(params[:id])
   end

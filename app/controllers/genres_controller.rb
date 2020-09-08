848ff1b7e4a5b97040809ff506c2d58b31c51c87
AaronJHarvey <61170939+AaronJHarvey@users.noreply.github.com>
2020-09-08 19:25:27 -0400

    fills in basic controller methods

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -8,28 +8,38 @@ class GenresController < ApplicationController
   end
 
   def new
-    @genre = Genre.find(params[:id])
+    @genre = Genre.new
   end
 
   def create
-    #uses the genre_params put into the form at new.html.erb
-    #if it the genre_params are valid it saves to the genre to db and redirects to the show page for that genre
-    #if it the genre_params are not valid it renders the new view for the user try creating a genre
+    @genre = Genre.new(genre_params)#uses the genre_params put into the form at new.html.erb
+    if @genre.save
+      redirect_to genre_path(@genre) ##THIS COULD BE @genre
+      #if it the genre_params are valid it saves to the genre to db and redirects to the show page for that genre
+    else
+      render :new #if it the genre_params are not valid it renders the new view for the user try creating a genre
+    end
   end
 
   def edit
-    #uses the inputted genre_id to look for a genre. If it finds one, sets the current genre(@genre) to the genre with that id
+    @genre = Genre.find(params[:id])#uses the inputted genre_id to look for a genre. If it finds one, sets the current genre(@genre) to the genre with that id
   end
 
   def update
-    #uses the inputted genre_id to look for a genre. If it finds one sets the current genre(@genre) to the genre with that id
-    #allows the user to make changes to the params for the genre (genre_params)
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+      if @genre.save
+        redirect_to genre_path(@genre) ##THIS COULD BE @genre
+        #uses the inputted genre_id to look for a genre. If it finds one sets the current genre(@genre) to the genre with that id
+      else
+        renders :edit#allows the user to make changes to the params for the genre (genre_params)
+      end
   end
 
   def destroy
-    #uses the genre_id that was input to set the working genre
-    #@genre.destroy
+    @genre = Genre.find(params[:id])#uses the genre_id that was input to set the working genre
+    @genre.destroy
     #throws up a message letting the user know the genre was deleted
-    #redirects to the index view (genres_path)
+    redirect_to genres_path #redirects to the index view (genres_path)
   end
 end
